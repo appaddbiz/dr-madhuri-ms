24187938461f7b77e00b7bb05b93f32cabaa8ef4
appaddbiz <appaddbiz@gmail.com>
2025-09-18 14:23:14 +0530

    (feat/google analytics):update code

--- app/layout.js
+++ app/layout.js
@@ -22,39 +22,53 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${dmSans.variable} ${manrope.variable}`}>
       <body>
         {children}
+
+        {/* jQuery Plugin */}
         <Script
           src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
           strategy="afterInteractive"
         />
         <Script id="appadd-plugin" strategy="afterInteractive">{`
-(function(){
-  function execute(){
-    try {
-      var eppathurl = window.location.origin + window.location.pathname;
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function(){
-        if (this.readyState === 4 && this.status === 200) {
-          if (this.response !== 0) {
-            var temp = this.response.split("||||||||||");
-            if (window.jQuery) {
-              jQuery('head').append(temp[0]);
-              jQuery('body').append(temp[1]);
-            } else {
-              document.head.insertAdjacentHTML('beforeend', temp[0]);
-              document.body.insertAdjacentHTML('beforeend', temp[1]);
+          (function(){
+            function execute(){
+              try {
+                var eppathurl = window.location.origin + window.location.pathname;
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function(){
+                  if (this.readyState === 4 && this.status === 200) {
+                    if (this.response !== 0) {
+                      var temp = this.response.split("||||||||||");
+                      if (window.jQuery) {
+                        jQuery('head').append(temp[0]);
+                        jQuery('body').append(temp[1]);
+                      } else {
+                        document.head.insertAdjacentHTML('beforeend', temp[0]);
+                        document.body.insertAdjacentHTML('beforeend', temp[1]);
+                      }
+                    }
+                  }
+                };
+                xhr.open('GET', atob('aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDg0ODIzNTkyNjMmZWtleXBhc3M9YXY4dFVidGxVdnExczMxS09vTjRoUTlqdW5xTWppeU04WXdGJnNpdGV1cmw9') + eppathurl);
+                xhr.send();
+              } catch (e) {
+                console.error('AppAdd plugin failed', e);
+              }
             }
-          }
-        }
-      };
-      xhr.open('GET', atob('aHR0cHM6Ly9wbHVnaW5zLmFwcGFkZC5pbi5uZXQvYWxsaGVhZGRhdGE/ZWtleT1lLUFQUEFERDg0ODIzNTkyNjMmZWtleXBhc3M9YXY4dFVidGxVdnExczMxS09vTjRoUTlqdW5xTWppeU04WXdGJnNpdGV1cmw9') + eppathurl);
-      xhr.send();
-    } catch (e) {
-      console.error('AppAdd plugin failed', e);
-    }
-  }
-  if (document.readyState === 'complete') { execute(); }
-  else { window.addEventListener('load', execute); }
-})();
+            if (document.readyState === 'complete') { execute(); }
+            else { window.addEventListener('load', execute); }
+          })();
+        `}</Script>
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QDDNNJJ07B"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QDDNNJJ07B');
         `}</Script>
       </body>
     </html>
